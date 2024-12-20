80ea76dcf05da1f57e0edc1a9ed0d5a40a079204
Freakyab <aryanbhisikar1@gmail.com>
2024-12-20 13:20:16 +0530

    refactor: remove unused imports and commented code in App component

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,11 @@ import Calendar from "./components/calendar";
 import Sidenav from "./components/sidenav";
 import CreateEvent from "./components/createEvent";
 import EventDetails from "./components/eventDetails";
-import { useEffect, useState } from "react";
-import useEvents from "./redux/useEvents";
+import { useState } from "react";
 
 function App() {
   const [updateEventId, setUpdateEventId] = useState(-1);
-  // const { AllDeleteEvent } = useEvents();
-  // useEffect(() => {
-  //   AllDeleteEvent();
-  // }, []);
+
   return (
     <div className="flex flex-col-reverse sm:flex-row bg-primary text-white">
       <Sidenav setUpdateEventId={setUpdateEventId} />
